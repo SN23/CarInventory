90f6707e3a450bf00afb7b80bfcfd6088a13df8a
Sukhjinder Nahal <SN23@users.noreply.github.com>
2017-09-12 21:47:15 -0400

    added method to retrieve car info from a vin

--- CarInventory/src/carinventory/Edmunds_API.java
+++ CarInventory/src/carinventory/Edmunds_API.java
@@ -21,7 +21,17 @@ public class Edmunds_API {
     private static class Fields{
         static final String NAME= "name";
         static final String MODELS= "models";
+        static final String MODEL= "model";
         static final String MAKES= "makes";
+        static final String MAKE= "make";
+        static final String ENGINE= "engine";
+        static final String CYLINDER= "cylinder";
+        static final String SIZE= "size";
+        static final String HORSEPOWER = "horsepower";
+        static final String DRIVEN_WHEELS = "drivenWheels";
+        static final String YEARS = "years";
+        static final String YEAR = "year";
+        static final String FUEL_TYPE = "type";
     }
     
     public static ArrayList<String> getMakes() {
@@ -102,7 +112,74 @@ public class Edmunds_API {
        
         return models;
     }
+        
+        
+        
+        public static CarVINInfo getCarInfo(String vin) {
+                
+        CarVINInfo carVINInfo = new CarVINInfo();
+        String url;
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://api.edmunds.com/api/vehicle/v2/vins/");
+        builder.append(vin);
+        builder.append("?fmt=json&api_key=");
+        builder.append(Config.API_KEY);
+       
+        url = (builder.toString());
+        
+        System.out.println(url);
+        
+        try {
+           carVINInfo = parseCarInfo(readUrl(url));
+        } 
+        catch (Exception ex) {
+            Logger.getLogger(Edmunds_API.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return carVINInfo;
+    }
     
+        public static CarVINInfo parseCarInfo(String buffer) {
+        
+        ArrayList<String> carInfo = new ArrayList<>();
+        CarVINInfo carVINInfo = new CarVINInfo();
+       
+        JSONObject car = new JSONObject(buffer);
+        JSONObject carMake = car.getJSONObject(Fields.MAKE);
+        String make = carMake.getString(Fields.NAME);
+        
+        JSONObject carModel = car.getJSONObject(Fields.MODEL);
+        String model = carModel.getString(Fields.NAME);
+        
+        JSONObject engine = car.getJSONObject(Fields.ENGINE);
+        int cylinderCount = engine.getInt(Fields.CYLINDER);
+        double engineSize = engine.getDouble(Fields.SIZE);
+        int horsepower = engine.getInt(Fields.HORSEPOWER);
+        String fuelType = engine.getString(Fields.FUEL_TYPE);
+        
+        String drivetrain = car.getString(Fields.DRIVEN_WHEELS);
+        
+        JSONArray carYear = car.getJSONArray(Fields.YEARS);
+        JSONObject yearObj = carYear.getJSONObject(0);
+        int year = yearObj.getInt(Fields.YEAR);
+        
+        
+        carVINInfo.setMake(make);
+        carVINInfo.setModel(model);
+        carVINInfo.setCylinderCount(Integer.toString(cylinderCount));
+        carVINInfo.setEngineSize(Double.toString(engineSize));
+        carVINInfo.setHorsepower(Integer.toString(horsepower));
+        carVINInfo.setYear(Integer.toString(year));
+
+        System.out.println("Make: " + make + "\n" +
+                           "Model: " + model + "\n" +
+                           "Cylinder Count: " +cylinderCount+ "\n" +
+                           "Engine Size: " + engineSize + "\n" +
+                           "Horsepower: " + horsepower + "\n" +
+                           "Driven Wheels: " + drivetrain + "\n" +
+                           "Year: " + year);
+        
+        return carVINInfo;
+    }
     
     
     private static String readUrl(String urlString) throws Exception {
