c411548fe5621f085622acf848d2c6876fe40e1f
Sukhjinder Nahal <SN23@users.noreply.github.com>
2017-09-12 21:48:21 -0400

    created carVINInfo to model data retrieved from edmunds for specified VIN

---
+++ CarInventory/src/carinventory/CarVINInfo.java
@@ -0,0 +1,83 @@
+package carinventory;
+
+/**
+ *
+ * @author Sukhjinder
+ */
+public class CarVINInfo {
+    
+    String make;
+    String model;
+    String cylinderCount;
+    String engineSize;
+    String horsepower;
+    String driveTrain;
+    String year;
+    String fuelType;
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCylinderCount() {
+        return cylinderCount;
+    }
+
+    public void setCylinderCount(String cylinderCount) {
+        this.cylinderCount = cylinderCount;
+    }
+
+    public String getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(String engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public String getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(String horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public String getDriveTrain() {
+        return driveTrain;
+    }
+
+    public void setDriveTrain(String driveTrain) {
+        this.driveTrain = driveTrain;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+    
+}
